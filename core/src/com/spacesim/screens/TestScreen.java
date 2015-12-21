3fe8618ced2d15b31429e52c6e5783b92e78aa03
nyxcharon <nyxcharon@gmail.com>
2015-12-21 18:16:50 -0500

    * Switch example to use Game/Screen classes

---
+++ core/src/com/spacesim/screens/TestScreen.java
@@ -0,0 +1,61 @@
+package com.spacesim.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by nyxcharon on 12/21/15.
+ */
+public class TestScreen implements Screen {
+
+    SpriteBatch batch;
+    Texture img;
+
+    public TestScreen () {
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
