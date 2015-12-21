7a3fb56e2131e97f2950d3aefabd921ba305cebb
nyxcharon <nyxcharon@gmail.com>
2015-12-21 17:45:20 -0500

    Adding default libGDX project

---
+++ desktop/src/com/spacesim/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.spacesim.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.spacesim.SpaceSim;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceSim(), config);
+	}
+}
