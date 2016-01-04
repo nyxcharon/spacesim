bb06530943bb484d61de879eb33877f072965cef
Bobby Martin <nyxcharon@gmail.com>
2016-01-03 22:28:18 -0500

    * Adding start screen w/ background.

---
+++ core/src/com/spacesim/screens/StartScreen.java
@@ -0,0 +1,61 @@
+package com.spacesim.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by Bobby on 1/3/2016.
+ */
+public class StartScreen implements Screen
+{
+    private Texture background;
+    private SpriteBatch batch;
+    public StartScreen()
+    {
+        background = new Texture(Gdx.files.internal("bg5.jpg"));
+        batch = new SpriteBatch();
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(background, 0, 0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
